e21795bfe0eda9510f6ebf18c4097a11ff21f6ac
奥金魔导师 <358369859@qq.com>
2018-08-21 12:36:24 +0800

    Weather，添加必要注释，以免忘记

--- weather/src/main/java/com/endymion/weather/MainActivity.java
+++ weather/src/main/java/com/endymion/weather/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 注意layout的文件名不能与其它模块中的一样，否则在产品模式下，可能会将其它模块的layout作为布局
         setContentView(R.layout.weather_activity_main);
         Toast.makeText(this, "weather", Toast.LENGTH_SHORT).show();
         TextView textView = findViewById(R.id.txt);
